463f880527b6c8347804258fa05e43f2fe9338cb
sparrow <alevar@gmail.com>
2015-11-27 21:50:55 -0600

    readding lost data

--- classes/cameraDetect.cpp
+++ classes/cameraDetect.cpp
@@ -1,1 +1,42 @@
-#include cameraDetect.h
+#include "cameraDetect.h"
+
+CameraDetect::CameraDetect(Mat* newFrame){
+	frame = newFrame;
+}
+
+int CameraDetect::approximateDistance(){
+
+}
+
+void CameraDetect::setCameraDistance(int*){
+
+}
+
+void CameraDetect::setHSV(int*,int*,int*,int*,int*,int*){
+
+}
+void CameraDetect::setHSV(int*,int*){
+
+}
+
+void CameraDetect::setResolution(int* width,int* height){
+	resolution = {width,hight};
+}
+
+float CameraDetect::getDistance(){
+	return results[0];
+}
+float CameraDetect::getLargestContour(){
+	return results[1];
+}
+
+void CameraDetect::noiseReduction(){
+	erode(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
+	dilate(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
+
+	dilate(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
+	erode(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
+}
+void CameraDetect::findLargestContour(){
+
+}
