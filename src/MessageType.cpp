f5b7a23a808e8e16b416658d93e202c242dcbdc7
sparrow <alevar@gmail.com>
2016-01-23 20:17:17 -0600

    Blank Type class indended for processing of application specific types. Will be used in encoding and decoding of the messages.

---
+++ src/MessageType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <stdlib.h>
+
+#include "MessageType.h"
+
+using namespace std;
+
+MessageType::MessageType(string type) {
+
+    
+}
---
+++ src/MessageType.h
@@ -0,0 +1,22 @@
+#ifndef MESSAGETYPE_H
+#define	MESSAGETYPE_H
+
+#include <string>
+
+using namespace std;
+
+enum TypeCode {
+    IMG,
+    HSV
+};
+
+class MessageType {
+    public:
+        MessageType(string);
+        
+    private:
+        TypeCode type;
+};
+
+#endif	/* MESSAGETYPE_H */
+
