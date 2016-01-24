bebeb8d6009a19f4c4dc56d069e22adad908b910
sparrow <alevar@gmail.com>
2016-01-23 20:16:07 -0600

    Blank ErrorCode class. Intended for work with application specific error codes.

---
+++ src/MessageError.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <stdlib.h>
+
+#include "MessageError.h"
+
+using namespace std;
+
+MessageError::MessageError(string errorcode) {
+
+    
+}
---
+++ src/MessageError.h
@@ -0,0 +1,22 @@
+#ifndef MESSAGEERROR_H
+#define	MESSAGEERROR_H
+
+#include <string>
+
+using namespace std;
+
+enum ErrorCode {
+    LOS,
+    INT
+};
+
+class MessageError {
+    public:
+        MessageError(string);
+        
+    private:
+        ErrorCode errorcode;
+};
+
+#endif	/* MESSAGEERROR_H */
+
