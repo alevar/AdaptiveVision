1b748fa09c588a269bfc9c557871f0c51c4c9e83
sparrow <alevar@gmail.com>
2016-01-24 18:42:25 -0600

    Class will provide micro-level insruction commands.

---
+++ src/ActionCode.cpp
@@ -0,0 +1,15 @@
+/*==================================================
+==================================================*/
+
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <stdlib.h>
+
+using namespace std;
+
+ActionCode::ActionCode(string code) {
+    
+}
---
+++ src/ActionCode.h
@@ -0,0 +1,17 @@
+/*==================================================
+==================================================*/
+
+#ifndef ACTIONCODE_H
+#define	ACTIONCODE_H
+
+#include <string>
+
+using namespace std;
+
+enum class ActionCode: int {
+    GET,
+    PUT
+};
+
+#endif	/* ACTIONCODE_H */
+
