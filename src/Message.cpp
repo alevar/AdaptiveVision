ccabd67d8d4a8246f39ec3e3c7fc5528b873a989
sparrow <alevar@gmail.com>
2016-02-09 23:50:08 -0600

    Refined feature composition and decomposition of messages

--- src/Message.cpp
+++ src/Message.cpp
@@ -15,6 +15,63 @@
 
 using namespace std;
 
+bool createMap(unordered_map<string, InstructionCode>& m) {
+    m["PUT"] = PUT;
+    m["GET"] = GET;
+    m["ACK"] = ACK;
+    m["DEFAULT"] = DEFAULT;
+    m["NEW_VALUE"] = NEW_VALUE;
+    m["NOTHING"] = NOTHING;
+    m["ERROR"] = ERROR;
+    m["READY"] = READY;
+    m["CONTINUE"] = CONTINUE;
+    m["CONFIRM"] = CONFIRM;
+    m["AGAIN"] = AGAIN;
+    m["LEN"] = LEN;
+    m["TIME"] = TIME;
+    return true;
+}
+
+unordered_map<string, InstructionCode> messageOpCodeMap;
+bool messageDummy = createMap(messageOpCodeMap);
+
+bool createArgMap(unordered_map<string, int>& m) {
+    m["PUT"] = 2;
+    m["GET"] = 2;
+    m["ACK"] = 0;
+    m["DEFAULT"] = 0;
+    m["NEW_VALUE"] = 2;
+    m["NOTHING"] = 0;
+    m["ERROR"] = 1;
+    m["READY"] = 0;
+    m["CONTINUE"] = 0;
+    m["CONFIRM"] = 1;
+    m["AGAIN"] = 1;
+    m["LEN"] = 1;
+    m["TIME"] = 1;
+    return true;
+}
+
+unordered_map<string, int> messageArgMap;
+bool messageDummy2 = createArgMap(messageArgMap);
+
+const char* messageOpCodeNames[] = {
+    "PUT",
+    "GET",
+    "ACK",        // Acknowledged a transaction
+    "DEFAULT",    // Default Values are set for the client
+    "NEW_VALUE",  // New Value available followed by the value itself
+    "NOTHING",    // Nothing New to Send
+    "ERROR",      // ERROR followed by the error code
+    "READY",      // READY to receive a package
+    "CONTINUE",   // Continue with another action?
+    "CONFIRM",    // Confirm that the task was handled correctly. Followed by the code of the task
+    "AGAIN",      // Try performing the last action again. Followed by the code of the action/task
+    "LEN",
+    "TIME"
+};
+
+
 Message::Message(){
 
 }
@@ -22,6 +79,12 @@ Message::Message(){
 Message::Message(string opcode) {
 
 	this->opcode = opcode;
+
+	MessageOpCode dummy(opcode);
+    MessageOpCode *inst;
+    inst = &dummy;
+    this->instOpcode = inst->MessageOpCode::getOpCode();
+    cout<<this->instOpcode<<endl;
 
 	this->outMessage = opcode;
 
@@ -47,7 +110,7 @@ Message::Message(string opcode, string message) {
 	this->outMessage = opcode;
 
 	if(MessageOpCode::numArgs(opcode) == 1){
-		outMessage = outMessage + "/1";
+		outMessage = outMessage + "/" + message;
 	}
 
 	if(MessageOpCode::numArgs(opcode) == 2){
@@ -81,7 +144,7 @@ MessageType Message::getType(){
 }
 
 string Message::getInfo(){
-	return this->tokens[2];
+	return this->tokens[tokens.size()-2];
 }
 
 uint32_t Message::getLength(){
@@ -116,6 +179,22 @@ void Message::toMessage(string stringData){
     inst = &dummy;
 
 	this->instOpcode = inst->MessageOpCode::getOpCode();
+
+	this->opcode = tokens[0];
+
+	this->outMessage = tokens[0];
+
+	cout << "CLASS: OPCODE " << tokens[0] << endl;
+
+	if(MessageOpCode::numArgs(tokens[0]) == 1){
+		outMessage = outMessage + "/1";
+		cout << "CLASS: 1 ARGUMENT" << endl;
+	}
+
+	if(MessageOpCode::numArgs(tokens[0]) == 2){
+		outMessage = outMessage + "/VAL/"+ to_string(tokens.back().size()) + "/" + tokens.back();
+		cout << "CLASS: 2 ARGUMENTS" << endl;
+	}
 }
 
 Message::~Message() {
