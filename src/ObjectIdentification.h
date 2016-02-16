93f4240609a3df8a9d770ce8432d57690e05b837
sparrow <alevar@gmail.com>
2016-02-15 21:08:26 -0600

    Added base procedure for Object presentation with argument "identify"

--- src/ObjectIdentification.h
+++ src/ObjectIdentification.h
@@ -4,6 +4,27 @@
 === Object Identification using machine learning tools
 ==================================================*/
 
+// http://aishack.in/tutorials/sift-scale-invariant-feature-transform-scale-space/
+// http://aishack.in/tutorials/canny-edge-detector/
+
+// Procedure
+/*=============================================================
+1. If an argument "identify" is supplied to the program at startup
+2. Start a countdown (5-1) & start capturing the image
+3. After the first countdown start a new countdown (5-1)
+4. Capture the new data: this time the new shape will be presented
+5. Start the new countdown (5-1) - signal for shape removal
+6. By image subtraction find the new shape to track
+=============================================================*/
+
+// Additional changes to the previous implementation
+/*=============================================================
+1. If an argument is given to the client
+2. When the connection between the client and the server is initiated
+3. Submit the final shape to the server
+4. Server will be using that shape for object tracking
+=============================================================*/
+
 #ifndef OBJECTIDENTIFICATION_H
 #define	OBJECTIDENTIFICATION_H
 
