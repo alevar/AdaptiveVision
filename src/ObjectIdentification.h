6203a93a424ef258faac287b98ede8445c41bbd8
sparrow <alevar@gmail.com>
2016-02-16 22:39:24 -0600

    Additional notes on the execution routine

--- src/ObjectIdentification.h
+++ src/ObjectIdentification.h
@@ -6,13 +6,20 @@
 
 // http://aishack.in/tutorials/sift-scale-invariant-feature-transform-scale-space/
 // http://aishack.in/tutorials/canny-edge-detector/
+// http://www.jayrambhia.com/blog/sift-keypoint-matching-using-python-opencv/
+// http://docs.opencv.org/2.4/doc/tutorials/features2d/feature_flann_matcher/feature_flann_matcher.html
 
 // Procedure
 /*=============================================================
-1. If an argument "identify" is supplied to the program at startup
+1. If an argument "identify" is supplied to the client program at startup
 2. Start a countdown (5-1) & start capturing the image
 3. After the first countdown start a new countdown (5-1)
 4. Capture the new data: this time the new shape will be presented
+	=> the shape should be of a specific color at the calibration
+	=> the color would not matter afterwards during the execution
+		-> during the execution of the program the shape will be matched using SIFT/FLANN keypoint matching
+		-> the new Values will be calculated from the histogram based on the isolated object
+
 5. Start the new countdown (5-1) - signal for shape removal
 6. By image subtraction find the new shape to track
 =============================================================*/
@@ -21,6 +28,8 @@
 /*=============================================================
 1. If an argument is given to the client
 2. When the connection between the client and the server is initiated
+	=> Communicate the fact that calibration needs to happen to the server right after the connection is established
+
 3. Submit the final shape to the server
 4. Server will be using that shape for object tracking
 =============================================================*/
