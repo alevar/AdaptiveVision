44445a508b95771ef684a09f0f4bcb20bfec2fe9
sparrow <alevar@gmail.com>
2016-02-21 20:47:59 -0600

    Class intended for creation of Templates. Templates are used against the incoming images, to identify similar objects

---
+++ src/Template.cpp
@@ -0,0 +1,43 @@
+/*==================================================
+==================== TEMPLATE CLASS ================
+====================================================
+===== Class intended for creation of Templates =====
+===== Templates are used against the incoming  =====
+===== images, to identify similar objects	   =====
+==================================================*/
+
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+
+#include "Template.h"
+
+using namespace std;
+using namespace cv;
+
+Template::Template(){
+}
+
+Template::Template(Mat image) {
+}
+
+Template::~Template() {
+}
