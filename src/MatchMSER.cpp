b98cff994b2339ff7ca16dc9f32e3fe1b715a08b
sparrow <alevar@gmail.com>
2016-03-11 00:52:57 -0600

    in development. To be used for finding matches between templates and frames

---
+++ src/MatchMSER.cpp
@@ -0,0 +1,332 @@
+/*==================================================
+=================== MATCH MSER CLASS ===============
+====================================================
+===== Class intended for identification of     =====
+===== Matches between Templates and the input  =====
+==================================================*/
+
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+
+#include "MatchMSER.h"
+#include "Template.h"
+
+using namespace std;
+using namespace cv;
+
+MatchMSER::MatchMSER(){
+	this->mserDetector(this->Params.delta, this->Params.minArea, this->Params.maxArea,this->Params.maxVariation,this->Params.minDiversity,
+			this->Params.maxEvolution,this->Params.areaThreshold, this->Params.minMargin, this->Params.edgeBlurSize);
+}
+
+MatchMSER::MatchMSER(Template *imgTPL){
+	this->imgTPL = imgTPL;
+	this->mserDetector(this->Params.delta, this->Params.minArea, this->Params.maxArea,this->Params.maxVariation,this->Params.minDiversity,
+			this->Params.maxEvolution,this->Params.areaThreshold, this->Params.minMargin, this->Params.edgeBlurSize);
+}
+
+void MatchMSER::setTemplate(Template *imgTPL){
+	this->imgTPL = imgTPL;
+}
+
+void MatchMSER::setParams(int *delta,int *minArea,int *maxArea,double *maxVariation,double *minDiversity,
+							int *maxEvolution,double *areaThreshold,double *minMargin,int *edgeBlurSize){
+
+	this->Params.delta 			= 	*delta;
+	this->Params.minArea 		= 	*minArea;
+	this->Params.maxArea 		= 	*maxArea;
+	this->Params.maxVariation 	= 	*maxVariation;
+	this->Params.minDiversity 	= 	*minDiversity;
+	this->Params.maxEvolution 	= 	*maxEvolution;
+	this->Params.areaThreshold 	= 	*areaThreshold;
+	this->Params.minMargin 		= 	*minMargin;
+	this->Params.edgeBlurSize 	= 	*edgeBlurSize;
+
+	this->mserDetector(*delta,*minArea,*maxArea,*maxVariation,*minDiversity,*maxEvolution,*areaThreshold,*minMargin,*edgeBlurSize);
+}
+
+// FUNCTIONS FOR CALCULATING MSERS AND FINDING MATCHES
+
+void MatchMSER::detectRegions(Mat &gray, vector<vector<Point> > & vectorX){
+    mserDetector(gray, vectorX);
+}
+
+Mat MatchMSER::mserToMat(vector<Point> *mser)
+{
+    int minX = min_element(mser->begin(), mser->end(), [] (Point &p1, Point &p2) { return p1.x < p2.x; })[0].x;
+    int minY = min_element(mser->begin(), mser->end(), [] (Point &p1, Point &p2) { return p1.y < p2.y; })[0].y;
+    int maxX = max_element(mser->begin(), mser->end(), [] (Point &p1, Point &p2) { return p1.x < p2.x; })[0].x;
+    int maxY = max_element(mser->begin(), mser->end(), [] (Point &p1, Point &p2) { return p1.y < p2.y; })[0].y;
+
+    Mat color(maxY - minY, maxX - minX, CV_8UC3);
+    
+    for_each(mser->begin(), mser->end(), [&] (Point &p) 
+    {
+        Point newPoint = Point(p.x - minX, p.y - minY);
+        line(color, newPoint, newPoint, WHITE);
+    });    
+    Mat gray;
+    cvtColor(color, gray, CV_BGRA2GRAY);
+    
+    return gray;
+}
+
+int MatchMSER::findHoles(Mat *img){
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(*img, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+    
+    if (hierarchy.size() == contours.size()) return 1;
+    
+    int result = 0;
+    for (size_t i = 0; i < hierarchy.size(); i++) { 
+        if (hierarchy[i][3] != - 1) result++;
+    }
+    
+    return result;
+}
+
+int MatchMSER::skeletLength(Mat *mserImg)
+{
+    Mat img;
+    mserImg->copyTo(img);
+    
+    threshold(img, img, 127, 255, THRESH_BINARY); 
+    Mat skel(img.size(), CV_8UC1, Scalar(0));
+    Mat temp;
+    Mat eroded;
+    
+    Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
+    bool done;      
+    do
+    {
+        erode(img, eroded, element);
+        dilate(eroded, temp, element); // temp = open(img)
+        subtract(img, temp, temp);
+        bitwise_or(skel, temp, skel);
+        eroded.copyTo(img);
+        
+        done = (countNonZero(img) == 0);
+    } while (!done);
+    
+    return countNonZero(skel);
+}
+
+double MatchMSER::contourAreas(Mat *img)
+{
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(*img, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    
+    for (size_t i = 0; i < hierarchy.size(); i++) { 
+        if (hierarchy[i][3] == -1) {
+            double area = contourArea(contours[i]);
+            if (area > 0) return area;
+        }
+    }
+    
+    return 0.0;
+}
+
+Features MatchMSER::extractFeature(vector<Point> *mser){
+
+    Features result;
+
+    Mat mserImg = mserToMat(mser);
+
+    if (mserImg.cols <= 2 || mserImg.rows <= 2){
+        cout << "null" << endl; 
+    }
+
+    RotatedRect minRect = minAreaRect(*mser);
+
+    int numberOfHoles = findHoles(&mserImg);
+    cout << "numberOfHoles: " << numberOfHoles << endl;
+
+    vector<Point> convexHull;
+
+    cv::convexHull(*mser, convexHull);
+    double convexHullAreaRate = (double)mser->size() / contourArea( convexHull );
+    cout << "convexHullAreaRate: " << convexHullAreaRate << endl;
+
+    double minRectAreaRate = (double)mser->size() / (double) minRect.size.area();
+
+    cout << "minRectAreaRate: " << minRectAreaRate << endl;
+
+    int leng = skeletLength(&mserImg);
+    double skeletLengthRate = (double)leng / (double) mser->size();
+
+    cout << "skeletLengthRate: " << skeletLengthRate << endl;
+
+    double contourArea = contourAreas(&mserImg);
+    if (contourArea == 0.0){
+        return result;
+    }
+    double contourAreaRate = (double)mser->size() / contourArea;
+    if (contourAreaRate > 1.0){
+        return result;
+    }
+    cout << "contourAreaRate: " << contourAreaRate << endl;
+
+    // vector<double> result = {(double)numberOfHoles,convexHullAreaRate,minRectAreaRate,skeletLengthRate,contourAreaRate};
+
+    result.numberOfHoles = (double)numberOfHoles;
+    result.convexHullAreaRate = convexHullAreaRate;
+    result.minRectAreaRate = minRectAreaRate;
+    result.skeletLengthRate = skeletLengthRate;
+    result.contourAreaRate = contourAreaRate;
+    result.full = true;
+
+    return result;
+}
+
+bool MatchMSER::matchTemplate(Features featuresTPL, Features featuresMSER)
+{
+    if (featuresTPL.numberOfHoles != featuresMSER.numberOfHoles) { 
+        return false; 
+    }    
+    if ((featuresTPL.convexHullAreaRate - featuresMSER.convexHullAreaRate) > 0.05) {
+        return false;
+    }
+    if ((featuresTPL.minRectAreaRate - featuresMSER.minRectAreaRate) > 0.05) {
+        return false;
+    }
+    if ((featuresTPL.skeletLengthRate - featuresMSER.skeletLengthRate) > 0.02) {
+        return false;
+    }
+    if ((featuresTPL.contourAreaRate - featuresMSER.contourAreaRate) > 0.1) {
+        return false;
+    }
+    
+    return true;
+}
+
+double MatchMSER::distace(Features *featuresTPL, Features *featuresMSER)
+{
+    return 
+    (featuresTPL->numberOfHoles == featuresMSER->numberOfHoles ? 1 : 10) * 
+    (log(fabs(featuresTPL->convexHullAreaRate - featuresMSER->convexHullAreaRate)) + 
+     log(fabs(featuresTPL->minRectAreaRate - featuresMSER->minRectAreaRate)) + 
+     log(fabs(featuresTPL->skeletLengthRate - featuresMSER->skeletLengthRate)) + 
+     log(fabs(featuresTPL->contourAreaRate - featuresMSER->contourAreaRate)));
+}
+
+Mat MatchMSER::processImage(Mat &image, Features featuresTPL){
+    Mat gray;
+    cout << "START PROCESSIMAGE" << endl;
+    cvtColor(image, gray, CV_BGRA2GRAY);
+    cout << "STOP PROCESSIMAGE" << endl;
+    vector<vector<Point> > msers;
+    detectRegions(gray,msers);
+    vector<Point> *bestMser = NULL;
+    double bestPoint = 10.0;
+
+    for_each(msers.begin(), msers.end(), [&] (vector<Point> &mser) 
+    {
+        Features featuresMSER = extractFeature(&mser);
+
+        cout << "FEATURES: " << featuresMSER.numberOfHoles << "\t"
+                            << featuresMSER.convexHullAreaRate << "\t"
+                            << featuresMSER.minRectAreaRate << "\t"
+                            << featuresMSER.skeletLengthRate << "\t"
+                            << featuresMSER.contourAreaRate << endl;
+
+        if(featuresMSER.full)            
+        {
+
+            cout << "ALL good" << endl;
+
+            bool wellMatched = matchTemplate(featuresTPL,featuresMSER);
+
+            if(wellMatched)
+            {
+
+                cout << "WELL MATCHED" << endl;
+                double tmp = distance(&featuresTPL, &featuresMSER);
+                cout << "TMP ================================= " << tmp << endl;
+                if ( bestPoint > tmp ) {
+                    bestPoint = tmp;
+                    bestMser = &mser;
+                }
+            }
+        }
+    });
+
+    if (bestMser)
+    {
+                
+        Rect bound = boundingRect(*bestMser);
+        rectangle(image, bound, GREEN, 3);
+    }
+
+    // if (msers.size() == 0) { 
+    //     return image; 
+    // };
+
+    return image;
+}
+
+vector<Point> MatchMSER::maxMser(Mat *gray)
+{
+    vector<vector<Point> > msers;
+
+    detectRegions (*gray, msers);
+
+    if (msers.size() == 0) return vector<Point>();
+    
+    vector<Point> mser = max_element(msers.begin(), msers.end(), [] (vector<Point> &m1, vector<Point> &m2) {
+        return m1.size() < m2.size();
+    })[0];
+    
+    return mser;
+}
+
+Mat MatchMSER::getPerspectiveMatrix(Point2f points[], Size2f size)
+{
+    Point2f dst[] = { 
+        Point2f(0, 0), 
+        Point2f(size.width, 0), 
+        Point2f(size.width, size.width), 
+        Point2f(0, size.width) 
+    };
+    bool first = PYTHAGOR(points[0], points[1]) < PYTHAGOR(points[1], points[2]);
+    Point2f src[] = {
+        first ? points[0] : points[1],
+        first ? points[1] : points[2],
+        first ? points[2] : points[3],
+        first ? points[3] : points[0]
+    };
+    
+    Mat result = getPerspectiveTransform(src, dst);
+    return result;
+}
+
+Mat MatchMSER::normalizeImage(Mat *image, Mat *M ,float size)
+{
+    Mat dst(Size(size, size), image->type());
+    warpPerspective(*image, dst, *M, dst.size(), INTER_LINEAR, BORDER_DEFAULT, Scalar(1, 1, 1));
+
+    return dst;
+}
+
+MatchMSER::~MatchMSER() {
+}
---
+++ src/MatchMSER.h
@@ -0,0 +1,97 @@
+/*==================================================
+==================== TEMPLATE CLASS ================
+====================================================
+===== Class intended for creation of Templates =====
+===== Templates are used against the incoming  =====
+===== images, to identify similar objects	   =====
+==================================================*/
+
+#ifndef MATCHMSER_H
+#define	MATCHMSER_H
+
+#include <string>
+#include <vector>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+
+#include "Template.h"
+
+using namespace std;
+using namespace cv;
+
+#define PYTHAGOR(p1,p2) (sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y)))
+
+struct Features {
+	int numberOfHoles;
+	double convexHullAreaRate;
+	double minRectAreaRate;
+	double skeletLengthRate;
+	double contourAreaRate;
+	bool full = false;
+};
+
+struct Colors {
+    const Scalar RED = Scalar(0, 0, 255);
+    const Scalar GREEN = Scalar(0, 255, 0);
+    const Scalar BLUE = Scalar(255, 0, 0);
+    const Scalar BLACK = Scalar(0, 0, 0);
+    const Scalar WHITE = Scalar(255, 255, 255);
+    const Scalar YELLOW = Scalar(0, 255, 255);
+    const Scalar LIGHT_GRAY = Scalar(100, 100, 100);
+};
+
+struct Params {
+	int delta 				= 5;
+	int minArea 			= 100;
+	int maxArea 			= 14400;
+	double maxVariation 	= 0.25;
+	double minDiversity 	= 0.2;
+	int maxEvolution 		= 200;
+	double areaThreshold 	= 1.01;
+	double minMargin 		= 0.003;
+	int edgeBlurSize 		= 0;
+};
+
+class MatchMSER {
+
+	public:
+
+		MatchMSER();
+		MatchMSER(Template*);
+		void setTemplate(Template*);
+		void setParams(int*,int*,int*,double*,double*,int*,double*,double*,int*);
+		virtual ~MatchMSER();	    
+	
+	private:
+		Template *imgTPL;
+		MserFeatureDetector mserDetector();
+
+		void detectRegions(Mat &, vector<vector<Point> > &);
+		Mat mserToMat(vector<Point>*);
+		int findHoles(Mat*);
+		int skeletLength(Mat*);
+		double contourAreas(Mat*);
+		Features extractFeature(vector<Point>*);
+		bool matchTemplate(Features, Features);
+		double distace(Features*, Features*);
+		Mat processImage(Mat &, Features);
+		vector<Point> maxMser(Mat*);
+		Mat getPerspectiveMatrix(Point2f[], Size2f);
+		Mat normalizeImage(Mat*, Mat*,float);
+
+};
+
+#endif	/* MATCHMSER_H */
+
